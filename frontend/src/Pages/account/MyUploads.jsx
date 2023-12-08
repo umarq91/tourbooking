dca1ea55267fcda801671e06d3d817cc79451298
noobDevUmar <m.umarqureshi091@gmail.com>
2023-12-08 07:44:49 -0800

    myuploads done

--- frontend/src/Pages/account/MyUploads.jsx
+++ frontend/src/Pages/account/MyUploads.jsx
@@ -5,29 +5,43 @@ import MyUploadsCard from '../../Components/Extra/MyUploadsCard'
 const MyUploads = () => {
 
 const [tours,setTours] = useState([])
+const [loading,setLoading] = useState(false)
 
     useEffect(()=>{
 
         const getData =async()=>{
+          setLoading(true)
           const {data}=  await axios.get('/api/tour/myTours')
      
          setTours(data)
+         setLoading(false)
         }
         getData()
     },[])
+
+
   return (
-    <div className="bg-gray-200 pt-16">
+    <div className="bg-gray-200 pt-16 min-h-screen">
       
        <AccountNav/>
-    <div className='mt-20 max-w-[90vw] md:max-w-[70vw]  mx-auto '>
+       {
+        loading? (
+          <div> 
+            <h1 className='text-center text-xl mt-16'> Loading </h1>
+            </div>
+        ):(
+          <div className='mt-20 max-w-[90vw] md:max-w-[70vw]  mx-auto '>
       
-    <h1 className="text-3xl font-poppins font-semibold "> My Uploads </h1>
-
-    {
-  tours.map((tour) => <MyUploadsCard tour={tour} />)
-}
-
-      </div>
+          <h1 className="text-3xl font-poppins font-semibold "> My Uploads </h1>
+      
+          {
+        tours.map((tour) => <MyUploadsCard tour={tour} />)
+      }
+      
+            </div>
+        )
+       }
+   
     </div>
   )
 }
