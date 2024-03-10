1301b2486465114d7ab2ef344c69cbc8bca278d1
M Umar Qureshi <134086072+umarq91@users.noreply.github.com>
2024-03-10 12:00:30 +0500

    Update SignInPage.jsx

--- frontend/src/Pages/auth/SignInPage.jsx
+++ frontend/src/Pages/auth/SignInPage.jsx
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
 export default function SignIn() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-const [errorr,setErrorr] = useState('')
-
-
+  const [errorr, setErrorr] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,62 +22,47 @@ const [errorr,setErrorr] = useState('')
         email: formData.email,
         password: formData.password,
       });
-  
-      const data = response.data; // assuming your response object has a 'data' property
-  
+
+      const data = response.data;
+
       if (data.success === false) {
-        console.log("errorrrrrrr");
         setError(true);
         setLoading(false);
-        setErrorr(data.message); // Set the error message here
+        setErrorr(data.message);
       } else {
-        window.location.href = '/'; // Redirect to the index page on successful login
+        navigate('/'); // Using navigate instead of direct window location change
       }
-  
+
     } catch (error) {
       setLoading(false);
       setError(true);
       setErrorr(error.response.data.message || "something went Wrong");
     }
   };
-  
-  
 
   return (
-    <div className='p-3 max-w-lg mx-auto mt-20'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          type='email'
-          placeholder='Email'
-          id='email'
-          className='bg-slate-100 p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          id='password'
-          className='bg-slate-100 p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <button
-         
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
-         {loading ? 'Loading...': 'Sign In'}
-        </button>
-        {/* <OAuth /> */}
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont Have an account?</p>
-        <Link to='/sign-up'>
-          <span className='text-blue-500'>Sign up</span>
-        </Link>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+     
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@yourmail.com" onChange={handleChange} required />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleChange} required />
+              </div>
+
+              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{loading ? 'Loading...' : 'Sign in'}</button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">Don’t have an account yet? <Link to='/sign-up' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link></p>
+            </form>
+          </div>
+        </div>
       </div>
-      <p className='text-red-700 mt-5'>
-        {error&& errorr }
-      </p>
-    </div>
+    </section>
   );
 }
